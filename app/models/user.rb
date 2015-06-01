860447cbec6fce7e7ab65065e3c009dc1a232621
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-06-01 13:27:01 -0700

    Save login/logout work

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine::cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
 end
